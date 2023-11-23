3b8b9f2d308ebc8b8136cd0db8590a2cba64319b
Jonas <92295347+jpx40@users.noreply.github.com>
2023-11-23 22:40:01 +0100

    update

--- server/pkg/router/router.go
+++ server/pkg/router/router.go
@@ -1,15 +1,46 @@
 package router
 
 import (
+	"net/http"
+
+	"github.com/olahol/melody"
+
+	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/jpx40/wdc/pkg/tmpl"
 )
 
 func Router() {
 	r := chi.NewRouter()
+	m := melody.New()
+
+	defer m.Close()
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+	r.Get("/", templ.Handler(tmpl.Index(tmpl.SimpleChat())).ServeHTTP)
+
+	r.Get("/ws", func(w http.ResponseWriter, r *http.Request) {
+		m.HandleRequest(w, r)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	http.ListenAndServe(":3000", r)
 }
--- server/pkg/router/template_handler.go
+++ server/pkg/router/template_handler.go
@@ -1,24 +1,24 @@
 package router
 
-import (
-	"github.com/a-h/templ"
-	"github.com/jpx40/wdc/pkg/template"
-	"github.com/labstack/echo/v4"
-)
-
-// indexHandler is the handler for the index route.
-
-func (e *echo.Echo)indexHandler(c echo.Context) error {
-	cmp := template.Index()
-	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
-	return cmp.Render(c.Request().Context(), c.Response().Writer)
-}
-
-func HTML(c echo.Context, cmp templ.Component) error {
-	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
-	return cmp.Render(c.Request().Context(), c.Response().Writer)
-}
-func html(cmp templ.Component) error {
-	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
-	return cmp.Render(c.Request().Context(), c.Response().Writer)
-}
+// import (
+// 	"github.com/a-h/templ"
+// 	"github.com/jpx40/wdc/pkg/template"
+// 	"github.com/labstack/echo/v4"
+// )
+//
+// // indexHandler is the handler for the index route.
+//
+// func (e *echo.Echo)indexHandler(c echo.Context) error {
+// 	cmp := template.Index()
+// 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+// 	return cmp.Render(c.Request().Context(), c.Response().Writer)
+// }
+//
+// func HTML(c echo.Context, cmp templ.Component) error {
+// 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+// 	return cmp.Render(c.Request().Context(), c.Response().Writer)
+// }
+// func html(cmp templ.Component) error {
+// 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+// 	return cmp.Render(c.Request().Context(), c.Response().Writer)
+// }
