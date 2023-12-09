eb676209d402370defdbb8e565c5e4b0313c5b6f
Jonas <92295347+jpx40@users.noreply.github.com>
2023-12-09 20:53:32 +0100

    update

--- server/main.go
+++ server/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	_ "github.com/IBM/fp-go/function"
 	"github.com/jpx40/wdc/pkg/router"
+	_ "github.com/markbates/goth"
+	_ "github.com/phakornkiong/go-pattern-match/pattern"
+	_ "github.com/sourcegraph/conc"
 )
 
 func main() {
